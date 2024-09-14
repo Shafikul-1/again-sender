c1f1215d3563809bdd5ba5569754449f55477782
shafik <shaifk108@gmail.com>
2024-09-14 16:12:31 +0600

    template

--- resources/views/mail/template.blade.php
+++ resources/views/mail/template.blade.php
@@ -1,70 +1,107 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .business-card {
+            display: flex;
+            width: 600px;
+            height: 200px;
+            background-color: white;
+            border-radius: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
+            border: 10px solid #f16425;
+        }
 
+        .left-side {
+            flex: 1;
+            background-color: #f16425;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 20px;
+        }
+
+        .photo-container img {
+            width: 100%;
+            border-radius: 20px;
+        }
+
+        .right-side {
+            flex: 2;
+            padding: 0px 0px 0px 23px;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .flex {
+            display: flex;
+            gap: 4;
+        }
+        .socialLinki{
+            margin: 0 2px;
+        }
+        .link{
+            text-decoration: none;
+            color: black;
+        }
+        .link:hover{
+            text-decoration: underline;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="email-container">
-        <div class="content">
-            <span> {!! $sending_email_data->mail_body !!}</span>
-            <br> <br> <br>
 
-            <div class="card"
-                style="background-color: white; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); display: flex; padding: 20px; width: 600px; align-items: center; justify-content: space-between;">
-                <div class="profile-pic"
-                    style="border-radius: 50%; overflow: hidden; width: 120px; height: 120px; border: 4px solid #87CEFA;">
-                    <img src="https://img.mysignature.io/p/3/b/8/3b85d994-3ac5-5255-b9df-8f5b3328829f.png?time=1709046225"
-                        alt="Leslie Johns" style="width: 100%; height: auto;">
-                </div>
-                <div class="info"
-                    style="display: flex; flex-direction: column; justify-content: center; margin-left: 20px; flex-grow: 1;">
-                    <h2 style="font-size: 22px; color: #333; margin: 0; text-transform: uppercase;">
-                        {{ $mail_sender_name }} <span style="font-weight: normal; font-size: 16px; color: #666;"> |
-                            Senior Product Manager</span>
-                    </h2>
-                    <ul class="contact" style="list-style: none; padding: 0; margin: 10px 0;">
-                        <li style="display: flex; align-items: center; margin-bottom: 5px;">
-                            <a href="mailto:{{ $mail_from }}">
-                                <img src="https://img.icons8.com/?size=256&id=7rhqrO588QcU&format=png" alt="email"
-                                    style="width: 20px; margin-right: 10px;"> {{ $mail_from }}
-                            </a>
-                        </li>
-                        @if ($sender_number)
-                            <li style="display: flex; align-items: center; margin-bottom: 5px;">
-                                <a href="tel:{{ $sender_number }}">
-                                    <img src="https://img.icons8.com/?size=64&id=44034&format=png" alt="phone"
-                                        style="width: 20px; margin-right: 10px;"> {{ $sender_number }}
-                                </a>
-                            </li>
-                        @endif
-                        @if ($sender_website)
-                            <li style="display: flex; align-items: center;">
-                                <a href="{{ $sender_website }}" style="text-decoration: none; color: black">
-                                    <img src="https://img.icons8.com/?size=48&id=63807&format=png" alt="website"
-                                        style="width: 20px; margin-right: 10px;"> {{ $sender_website }}
-                                </a>
-                            </li>
-                        @endif
-                    </ul>
-                    @if ($other_links)
-                        <div class="social-icons" style="margin-top: 10px;">
-                            @foreach ($other_links as $links)
-                                <a href="{{ $links['yourLink'] }}">
-                                    <img src="{{ $links['iconLink'] }}" style="width: 30px; margin-right: 10px;">
-                                </a>
-                            @endforeach
-                        </div>
-                    @endif
+    <span> {!! $sending_email_data->mail_body !!}</span>
 
-                </div>
+    <div class="business-card">
+        <div class="left-side">
+            <div class="photo-container">
+                <img src="https://lh3.googleusercontent.com/a/ACg8ocIhHmH8cqQbGoJeurXszjZcVFG5G3i4JF65PwWZ93piNN63Qy4=s288-c-no"
+                    alt="">
             </div>
-
         </div>
-
+        <div class="right-side">
+            <div class="details">
+                <div class="flex " style="margin-top: 15px;">
+                    <h3 style="margin: 0; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+                          {{ $mail_sender_name }} </h3>
+                    <span style="margin-left: 5px; font-weight: bolder;">||</span>
+                    <span style="margin-left: 4px; font-size: 15px; margin-top: 0px; font-family: cursive;">Senior Product Manager</span>
+                </div>
+                @if ($sender_number)
+                    <div style=" display: flex; margin-top: 15px;">
+                        <img src="https://img.icons8.com/?size=64&id=44034&format=png"
+                            style="width: 20px; height: 20px; margin: 0;" alt="">
+                        <a href="tel:{{ $sender_number }}" class="link" style=" margin-left: 5px;">+{{ $sender_number }}</a>
+                    </div>
+                @endif
+                    <div style=" display: flex; margin-top: 5px;">
+                        <img src="https://img.icons8.com/?size=80&id=68248&format=png"
+                            style="width: 20px; height: 20px; margin: 0;" alt="">
+                        <a href="mailto:{{ $mail_from }}" class="link" style=" margin-left: 5px;">{{ $mail_from }}</a>
+                    </div>
+                @if ($sender_website)
+                    <div style="display: flex; margin-top: 5px;">
+                        <img src="https://img.icons8.com/?size=80&id=8bVNpI807DcA&format=png"
+                            style="width: 20px; height: 20px; margin: 0;" alt="">
+                        <a href="{{ $sender_website }}" class="link" style="margin-left: 5px; "> {{ $sender_website }}</a>
+                    </div>
+                @endif
+            </div>
+            @if ($other_links)
+                <div class="flex" style="margin-top: 15px;">
+                    @foreach ($other_links as $links)
+                        <a href="{{ $links['yourLink'] }}" class="socialLinki">
+                            <img src="{{ $links['iconLink'] }}" alt="" width="20px">
+                        </a>
+                    @endforeach
+                </div>
+            @endif
+        </div>
     </div>
 </body>
 
